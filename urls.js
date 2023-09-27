57c6efdb6380e354481aa9e1ffddc9a08612f784
Audrey <audrey_reynaud@hotmail.fr>
2023-09-27 18:03:32 +0200

    mymovies

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = "https://moviz-backend-pearl.vercel.app/";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = "https://moviz-frontend-ten.vercel.app/";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
